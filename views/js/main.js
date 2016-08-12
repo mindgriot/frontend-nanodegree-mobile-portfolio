a55c3d891d02c4dbe04e44e42df47e721ba9e2dc
phutchins3 <phutchins3@gmail.com>
2016-08-12 17:26:00 -0400

    Performance
    Added imagemin compressed source for png file

--- views/js/main.js
+++ views/js/main.js
@@ -530,7 +530,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 23; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "dist/images/pizza-min.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
