4221df3d79004c11a07fb2a054096750c485d650
phutchins3 <phutchins3@gmail.com>
2016-08-13 05:38:45 -0400

    Performance
    Removing the document.querySelectorAll

--- views/js/main.js
+++ views/js/main.js
@@ -490,7 +490,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     // find all pizzas affected by this function
